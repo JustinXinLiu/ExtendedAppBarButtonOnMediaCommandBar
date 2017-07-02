55f52f90522ed88e4c021720388a61112ecb6d52
Justin Liu <justin.xin.liu@gmail.com>
2017-07-02 21:59:17 +1000

    First check-in.

---
+++ ExtendedAppBarButtonOnMediaCommandBar/ExtendedAppBarButton.cs
@@ -0,0 +1,108 @@
+﻿using Windows.System;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Automation.Peers;
+using Windows.UI.Xaml.Automation.Provider;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Shapes;
+
+namespace ExtendedAppBarButtonOnMediaCommandBar
+{
+    [TemplatePart(Name = "FocusVisual", Type = typeof(Rectangle))]
+    public class ExtendedAppBarButton : AppBarButton
+    {
+        private Rectangle _focusVisual;
+        private bool _ignoreGlobalSpaceKeyPress;
+
+        public ExtendedAppBarButton()
+        {
+            DefaultStyleKey = typeof(ExtendedAppBarButton);
+
+            AddHandler(KeyDownEvent, new KeyEventHandler(OnKeyDown), true);
+            AddHandler(KeyUpEvent, new KeyEventHandler(OnKeyUp), true);
+
+            GotFocus += OnGotFocus;
+            LostFocus += OnLostFocus;
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _focusVisual = (Rectangle)GetTemplateChild("FocusVisual");
+        }
+
+        public bool IsPlayPauseButton
+        {
+            get => (bool)GetValue(IsPlayPauseButtonProperty);
+            set => SetValue(IsPlayPauseButtonProperty, value);
+        }
+        public static readonly DependencyProperty IsPlayPauseButtonProperty = DependencyProperty.Register(
+            "IsPlayPauseButton", typeof(bool), typeof(ExtendedAppBarButton), new PropertyMetadata(false,
+                (s, e) =>
+                {
+                    var self = (ExtendedAppBarButton)s;
+                    var allowSpace = (bool)e.NewValue;
+
+                    if (allowSpace)
+                    {
+                        //self.AllowFocusOnInteraction = true;
+                        Window.Current.CoreWindow.KeyDown += self.OnWindowKeyDown;
+                    }
+                    else
+                    {
+                        //self.AllowFocusOnInteraction = false;
+                        Window.Current.CoreWindow.KeyDown -= self.OnWindowKeyDown;
+                    }
+                }));
+
+        private void OnWindowKeyDown(CoreWindow sender, KeyEventArgs args)
+        {
+            if (!_ignoreGlobalSpaceKeyPress && args.VirtualKey == VirtualKey.Space)
+            {
+                var ap = new ButtonAutomationPeer(this);
+                var ip = ap.GetPattern(PatternInterface.Invoke) as IInvokeProvider;
+                ip?.Invoke();
+            }
+        }
+
+        private void OnKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (!IsPlayPauseButton && e.Key == VirtualKey.Space)
+            {
+                IsEnabled = false;
+            }
+            else if (IsPlayPauseButton && e.Key == VirtualKey.Space)
+            {
+                _ignoreGlobalSpaceKeyPress = true;
+            }
+        }
+
+        private void OnKeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            if (!IsPlayPauseButton && e.Key == VirtualKey.Space)
+            {
+                IsEnabled = true;
+            }
+
+            _ignoreGlobalSpaceKeyPress = false;
+        }
+
+        private void OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (AllowFocusWhenDisabled)
+            {
+                _focusVisual.Opacity = 1;
+            }
+        }
+
+        private void OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (AllowFocusWhenDisabled)
+            {
+                _focusVisual.Opacity = 0;
+            }
+        }
+    }
+}
--- ExtendedAppBarButtonOnMediaCommandBar/MainPage.xaml.cs
+++ ExtendedAppBarButtonOnMediaCommandBar/MainPage.xaml.cs
@@ -1,30 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace ExtendedAppBarButtonOnMediaCommandBar
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+        }
+
+        private async void OnPickVideoClick(object sender, RoutedEventArgs e)
+        {
+            var openPicker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = PickerLocationId.VideosLibrary
+            };
+
+            openPicker.FileTypeFilter.Add(".mp4");
+            openPicker.FileTypeFilter.Add(".mkv");
+            openPicker.FileTypeFilter.Add(".avi");
+
+            var file = await openPicker.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                Media.Source = MediaSource.CreateFromStorageFile(file);
+                Media.MediaPlayer.Play();
+            }
         }
     }
 }
